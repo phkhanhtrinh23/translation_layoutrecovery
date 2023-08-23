f4b706d999ed7dc419160ffdc19eb7618bbdf01a
YangTuanAnh <yangtuananh2003@gmail.com>
2023-08-23 11:32:47 +0000

    change: editable name value

--- Frontend/app/history/page.tsx
+++ Frontend/app/history/page.tsx
@@ -27,17 +27,17 @@ const History = () => {
     };
 
     const getSearch = () => {
-        
+
         fetch("/history/api", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({query: search.replace(/\s/g, '' )})
+            body: JSON.stringify({ query: search.replace(/\s/g, '') })
         })
-        .then(res => res.json())
-        .then(data => {setSearchLinks(data.data); setSearching(true);})
-        .catch(err => toast("Internal error, try again"))
+            .then(res => res.json())
+            .then(data => { setSearchLinks(data.data); setSearching(true); })
+            .catch(err => toast("Internal error, try again"))
     }
     return (
         <div className="min-h-screen flex">
@@ -45,12 +45,11 @@ const History = () => {
             <main className="p-4 w-full overflow-y-scroll">
                 <h2>History</h2>
                 <div className="flex gap-2 my-2">
-                    <input type="text" id="search" placeholder="Search here" className="rounded p-2 w-full" 
-                    onChange={e => setSearch(e.target.value)}/>
+                    <input type="text" id="search" placeholder="Search here" className="rounded p-2 w-full"
+                        onChange={e => setSearch(e.target.value)} />
                     <button className="bg-sky-600" onClick={() => getSearch()}>Search</button>
                 </div>
-                {!searching ? 
-                <InfiniteScroll
+                {!searching ? (pdfLinks.length ? <InfiniteScroll
                     dataLength={pdfLinks.length}
                     next={loadMore}
                     hasMore={hasMore}
@@ -89,43 +88,44 @@ const History = () => {
                             )
                         })}
                     </table>
-                </InfiniteScroll> : 
-                <InfiniteScroll
-                dataLength={searchLinks.length}
-                next={loadMore}
-                hasMore={hasMore}
-                loader={<p>Loading...</p>}
-                endMessage={<p>No more PDFs</p>}
-            >
-                <table className="w-full text-center">
-                <tr>
-                            <th>File</th>
-                            <th>Language</th>
-                            <th>Datetime</th>
-                        </tr>
-                        {searchLinks.map((record, index) => {
-                            const timestamp = record.created_at;
-                            const [datePart, timePart] = timestamp.split("T")
-                            const parsedDate = new Date(datePart + "T" + timePart);
-                            const formattedDate = parsedDate.toISOString().replace("T", " ").slice(0, -5);
-                            return (
-                                <tr className="h-12">
-                                    <td>
-                                        <a href={record.file} target="_blank" rel="noopener noreferrer" className="hover:text-sky-600 hover:underline">
-                                            {record.file_name}
-                                        </a>
-                                    </td>
-                                    <td>{record.language === "vi" ? <span className="bg-green-600 text-white py-1 px-2 rounded-full">VI</span> :
-                                        (record.language === "en" ? <span className="bg-yellow-600 text-white py-1 px-2 rounded-full">EN</span> :
-                                            <span className="bg-red-600 text-white py-1 px-2 rounded-full">JP</span>)}</td>
-                                    <td><span>{formattedDate}</span></td>
-                                </tr>
-                            )
-                        })}
-                </table>
-                </InfiniteScroll>
+                </InfiniteScroll> : <p>No documents, try translating something</p>)
+                    : (searchLinks.length ? <InfiniteScroll
+                        dataLength={searchLinks.length}
+                        next={loadMore}
+                        hasMore={hasMore}
+                        loader={<p>Loading...</p>}
+                        endMessage={<p>No more PDFs</p>}
+                    >
+                        <table className="w-full text-center">
+                            <tr>
+                                <th>File</th>
+                                <th>Language</th>
+                                <th>Datetime</th>
+                            </tr>
+                            {searchLinks.map((record, index) => {
+                                const timestamp = record.created_at;
+                                const [datePart, timePart] = timestamp.split("T")
+                                const parsedDate = new Date(datePart + "T" + timePart);
+                                const formattedDate = parsedDate.toISOString().replace("T", " ").slice(0, -5);
+                                return (
+                                    <tr className="h-12">
+                                        <td>
+                                            <a href={record.file} target="_blank" rel="noopener noreferrer" className="hover:text-sky-600 hover:underline">
+                                                {record.file_name}
+                                            </a>
+                                        </td>
+                                        <td>{record.language === "vi" ? <span className="bg-green-600 text-white py-1 px-2 rounded-full">VI</span> :
+                                            (record.language === "en" ? <span className="bg-yellow-600 text-white py-1 px-2 rounded-full">EN</span> :
+                                                <span className="bg-red-600 text-white py-1 px-2 rounded-full">JP</span>)}</td>
+                                        <td><span>{formattedDate}</span></td>
+                                    </tr>
+                                )
+                            })}
+                        </table>
+                    </InfiniteScroll> : <p>Nothing found</p>)
+
                 }
-                <ToastContainer/>
+                <ToastContainer />
             </main>
         </div>
     )
