166feea07328eae4d270d1f9df2c8658c40d439f
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-08-24 16:25:28 +0000

    Update Docker Deployment

--- Backend/services/settings.py
+++ Backend/services/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,15 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3e$y!xq_wvo%4u7d5m9w7e*ptetlyi_pnm#@ipxa$7+ly$_j=g"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 # for instant deploying the website using ngrok platform
-CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app", "http://*.127.0.0.1", "http://0.0.0.0"]
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app", "http://127.0.0.1", "http://0.0.0.0"]
 
 DJANGO_ALLOWED_HOSTS='localhost 127.0.0.1 [::1] 0.0.0.0'
 # Application definition
--- Backend/setup.sh
+++ Backend/setup.sh
@@ -1,1 +1,1 @@
-gdown --id 1Jx2m_2I1d9PYzFRQ4gl82xQa-G7Vsnsl ; python -m pip install -e ../. ; python manage.py makemigrations account translation ; python manage.py migrate ; python manage.py runserver 0.0.0.0:8000
+gdown --id 1Jx2m_2I1d9PYzFRQ4gl82xQa-G7Vsnsl && python3 -m pip install -e ../. && python3 manage.py makemigrations account translation && python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
